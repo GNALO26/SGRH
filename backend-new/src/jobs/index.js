6006767b92cd85011bbf144fad265ac960ad1169
Alfred SOSSA <alfredsossa17@gmail.com>
2026-08-21 10:43:19 +0100

    jjsskl

--- backend-new/src/jobs/index.js
+++
@@ -1,6 +0,0 @@
-// Importe et initialise tous les processeurs de files d'attente
-require('./emailJob');
-require('./notificationJob');
-require('./absenceJob');
-
-module.exports = {};
--- frontend-new/android/app/src/main/java/com/sgrh/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.sgrh.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
